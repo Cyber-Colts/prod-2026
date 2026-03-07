13fa869b0c2ee86daa7708b90c35e85fd63e5296
randomx10 <account1@mindustry.me>
2026-03-07 00:31:27 -0500

    feat: AdvantageKit pt2

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,13 +7,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "prod-2026";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 16;
-  public static final String GIT_SHA = "581031e19fabe841abdecd4ba0bcad4e6e85196f";
-  public static final String GIT_DATE = "2026-03-06 23:40:21 EST";
+  public static final int GIT_REVISION = 17;
+  public static final String GIT_SHA = "0e047a9a12a8a897b949758947602e18648b8764";
+  public static final String GIT_DATE = "2026-03-07 00:29:39 EST";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2026-03-07 00:28:20 EST";
-  public static final long BUILD_UNIX_TIME = 1772861300720L;
-  public static final int DIRTY = 1;
+  public static final String BUILD_DATE = "2026-03-07 00:29:56 EST";
+  public static final long BUILD_UNIX_TIME = 1772861396855L;
+  public static final int DIRTY = 0;
 
   private BuildConstants(){}
 }
