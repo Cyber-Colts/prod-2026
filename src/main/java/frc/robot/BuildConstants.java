a01ffb52c4ff2798eea669dd0a3dd0a0198d1acf
Sebastian Ocampo <sebasop2031@gmail.com>
2026-03-09 16:54:06 -0500

    Modified gitignore, and Copilot Fix

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "prod-2026";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 21;
-  public static final String GIT_SHA = "ff540719213094492903012d8f38b6cfc323fc3d";
-  public static final String GIT_DATE = "2026-03-07 01:55:26 EST";
+  public static final int GIT_REVISION = 28;
+  public static final String GIT_SHA = "2e918de09d60d7cc58827c8b44facbe4e1d996ea";
+  public static final String GIT_DATE = "2026-03-09 17:15:15 EDT";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2026-03-07 18:43:46 EST";
-  public static final long BUILD_UNIX_TIME = 1772927026685L;
+  public static final String BUILD_DATE = "2026-03-09 17:44:23 EDT";
+  public static final long BUILD_UNIX_TIME = 1773092663698L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
