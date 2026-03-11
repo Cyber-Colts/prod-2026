4afd598f93fb65ed2f724faf344303583b47d16b
randomx10 <account1@mindustry.me>
2026-03-11 01:53:50 -0500

    fix: oops

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "prod-2026";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 34;
-  public static final String GIT_SHA = "9e7ba48c0d133f7fb4dc3649658fbcbc8af32bce";
-  public static final String GIT_DATE = "2026-03-11 02:08:29 EDT";
+  public static final int GIT_REVISION = 35;
+  public static final String GIT_SHA = "1b8e2de1d28a26c26ece328c85a5a7e1ee9b0432";
+  public static final String GIT_DATE = "2026-03-11 02:39:02 EDT";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2026-03-11 02:38:29 EDT";
-  public static final long BUILD_UNIX_TIME = 1773211109482L;
+  public static final String BUILD_DATE = "2026-03-11 02:53:03 EDT";
+  public static final long BUILD_UNIX_TIME = 1773211983906L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
