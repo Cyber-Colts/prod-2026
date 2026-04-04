b588e71ab6635b323996f594340896b0f08a38b3
randomx10 <account1@mindustry.me>
2026-04-04 00:24:17 -0500

    update vendordeps

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "prod-2026";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 43;
-  public static final String GIT_SHA = "6c9bc1ce7c06dedd9a8498c902691ea62114787f";
-  public static final String GIT_DATE = "2026-03-23 00:24:39 EDT";
+  public static final int GIT_REVISION = 47;
+  public static final String GIT_SHA = "d8bcc0c984d5b5ba008a71ca348baae594d68a93";
+  public static final String GIT_DATE = "2026-04-03 02:43:34 EDT";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2026-04-03 02:40:49 EDT";
-  public static final long BUILD_UNIX_TIME = 1775198449982L;
+  public static final String BUILD_DATE = "2026-04-04 01:00:19 EDT";
+  public static final long BUILD_UNIX_TIME = 1775278819466L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
